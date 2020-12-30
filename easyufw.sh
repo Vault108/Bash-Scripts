f07d137d31b73a6f94835aea5a13cd38253692c7
Vault108 <44564111+Vault108@users.noreply.github.com>
2020-12-30 03:20:44 -0500

    tweaked some stuff

--- easyufw.sh
+++ easyufw.sh
@@ -1,11 +1,20 @@
-echo -n "Allow or Deny: "
-read ad
-echo -n "Enter an ip to $ad: "
+echo -n "Enter a ip "
 read ip
-echo -n "UDP or TCP: "
-read ut
 echo -n "Enter the Port: "
 read port
+echo "Allow or Deny connections from $ip"
+  select yn in "Allow" "Deny"; do
+      case $yn in
+        Allow ) ad=allow; break;;
+        Deny  ) ad=deny;  break;;
+     esac
+  done
+echo "TCP or UDP?"
+  select yn in "TCP" "UDP"; do
+      case $yn in
+          TCP ) ut=tcp; break;;
+          UDP ) ut=udp; break;;
+     esac
+  done                                                                           
 sudo ufw $ad from $ip proto $ut to any port $port
-
 
